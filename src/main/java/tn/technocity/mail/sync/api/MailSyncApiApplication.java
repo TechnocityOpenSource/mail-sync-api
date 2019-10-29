4c82eb67f74419a4c714d373dc7bd78f30c93044
Sami Bourouis <sami.bourouis.pro@gmail.com>
2019-10-29 22:41:52 +0100

    Initial Commit

---
+++ src/main/java/tn/technocity/mail/sync/api/MailSyncApiApplication.java
@@ -0,0 +1,13 @@
+package tn.technocity.mail.sync.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSyncApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSyncApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/technocity/mail/sync/api/MailSyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.technocity.mail.sync.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSyncApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
