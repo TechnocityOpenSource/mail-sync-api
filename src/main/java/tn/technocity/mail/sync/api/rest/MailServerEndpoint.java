73e256baad7c76c338217fd5b9db4ad885304c0f
Sami Bourouis <sami.bourouis.pro@gmail.com>
2019-10-29 22:48:49 +0100

    Add MailServerEndpoint

---
+++ src/main/java/tn/technocity/mail/sync/api/rest/MailServerEndpoint.java
@@ -0,0 +1,10 @@
+package tn.technocity.mail.sync.api.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail-server")
+public class MailServerEndpoint {
+
+}
